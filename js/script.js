e65726ba87ec931a7ac06acda093f9bf4d5b6713
devjosegomez <devjosegomez@gmail.com>
2019-11-01 23:02:22 -0600

    Added eventListener, added querySelectors

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const output = document.querySelector("#output");
+const player1 = document.querySelector("#player1");
+const player2 = document.querySelector("#player2");
+const button = document.querySelector("button");
+
+button.addEventListener("click",() =>{
+    console.log(1);
+});
