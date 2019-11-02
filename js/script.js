d57d44f5999ba4b73705ce630443b748ece80a18
devjosegomez <devjosegomez@gmail.com>
2019-11-01 23:22:59 -0600

    Output innetHTML dices

--- js/script.js
+++ js/script.js
@@ -4,9 +4,22 @@ const player2 = document.querySelector("#player2");
 const button = document.querySelector("button");
 
 button.addEventListener("click",() =>{
+    let tmp="";
     console.log(1);
-    let rolls = [roll(6), roll(6)];
+    let rolls = [roll(6), roll(6)]; //index 0 Player(You) / index 1 Computer
     console.log(rolls);
+    if(rolls[0] == rolls[1]){
+        tmp = "Hey! it's a draw @.@";
+    }else if(rolls[0] > rolls[1]){
+        tmp = "Player wins!";
+    }else{
+        tmp = "Computer wins!"
+    }
+    
+    output.innerHTML = "<h1>Let's roll on!</h1>";
+    output.innerHTML += `<h2>${tmp}</h2>`;
+    player1.innerHTML = rolls[0];
+    player2.innerHTML = rolls[1];
 });
 
 function roll(n){
